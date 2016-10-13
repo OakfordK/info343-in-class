6c9f3d239aa96500eaa7a8993c772768431510f5
Kyle Williams-Smith <ninjagodzilla@gmail.com>
2016-10-13 12:22:03 -0700

    End of lecture on Oct 13

--- javascript/js/learning.js
+++ javascript/js/learning.js
@@ -41,7 +41,9 @@ console.group("PRACTICE: Variables and Basic Types");
 //Create another variable named `y`, assign it the value
 //currently held in the variable `x`, and write the value
 //of `y` to the console log
-
+var y = x;
+y = 10;
+console.log("X is " + x);
 
 //now assign `y` the numeric value 10
 //what does x contain now? Write it to the console
@@ -78,12 +80,16 @@ console.group("PRACTICE: Strings");
 //with some text, and then create another string `s4`
 //and assign it the concatenation of `s2` and `s3`
 //then write it to the console so you can verify it worked.
+var s2 = "cat"
+var s3 = "dog"
 
+var s4 = s2+s3;
+console.log("Cartoon show from the old days: " + s4);
 
 //use the `.trim()` method to remove the leading and
 //trailing white space from this string
 var withSpaces = "    trim those spaces!     ";
-
+console.log(withSpaces.trim());
 
 console.groupEnd();
 
@@ -158,7 +164,18 @@ console.group("PRACTICE: Objects");
 //now try adding a property named `web site` (with a space)
 //setting it to some string value...it's tricky...
 
-
+var course2 = {
+    department: "CSE",
+    number: "413",
+    teacher: {
+        fname: "Hal",
+        lname: "Perkins"
+    },
+    room: "MUE 156",
+    description: "Functional Programming"
+};
+course2["web site"] = "https://www.cs.washington.edu/413";
+console.log(course2);
 
 
 console.groupEnd();
@@ -199,7 +216,10 @@ console.group("PRACTICE: Arrays");
 //create another array of playing card suits
 //(clubs, diamonds, hearts, spades)
 
-
+var suits = ["clubs", "diamonds", "hearts", "spaces"];
+suits.push("jokers");
+console.log(suits);
+console.log(suits[suits.length-1]);
 //then add a new element named "jokers"
 //afer adding it, access it in the array
 //and log it to the console
@@ -287,6 +307,7 @@ console.groupCollapsed("Functions");
 function reverseString(s) {
     var reversed = "";
     var idx;
+    s = String(s);
     for (idx = s.length-1; idx >= 0; idx--) {
         //short form of reversed = reversed + s.charAt(idx)
         reversed += s.charAt(idx);
@@ -342,7 +363,12 @@ console.group("PRACTICE: Functions");
 //and returns the minimum of the two, or the first argument
 //if they are equal to each other. Then call it a few times
 //with various numbers to test it.
-
+var minimum = function(a, b) {
+    return a < b ? a : b;
+}
+console.log(minimum(1, 2));
+console.log(minimum(100, 10));
+console.log(minimum(100, 100));
 
 console.groupEnd();
 
@@ -417,6 +443,13 @@ function generateRandomNumbers(howMany, minimum, maximum) {
 }
 
 //>>> your code goes here!
+var a1 = generateRandomNumbers(100, 1, 100);
+var a2 = a1.sort(function(a, b) {
+    return a - b;
+});
+console.log(a2);
+var a3 = a2.map(function(a) { return a * a });
+console.log(a3);
 
 
 //now use the .sort() method on a generated array of random
